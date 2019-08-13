5ce1317f0a5befb0b8e2c5cfb0783f690fd0b85d
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-13 15:20:50 +0100

    Updates

---
+++ precache-manifest.54cb39fc6c248d10999a2f6a30000ab0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fe031cb26e4fc82fe7103899914538",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f77e9b59b72cdc56cb3e",
+    "url": "/todo/static/css/main.955228de.chunk.css"
+  },
+  {
+    "revision": "37eb9ae443cf3f4a88eb",
+    "url": "/todo/static/js/2.b7761a51.chunk.js"
+  },
+  {
+    "revision": "f77e9b59b72cdc56cb3e",
+    "url": "/todo/static/js/main.85af476b.chunk.js"
+  },
+  {
+    "revision": "263ea585d3801eaba5dc",
+    "url": "/todo/static/js/runtime~main.447bd38d.js"
+  }
+]);
